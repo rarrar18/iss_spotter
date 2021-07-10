5189345290385d4d08cefe262a4d4d40d16f9f23
Raphaella Robles <raphaella.robles@gmail.com>
2021-07-10 01:10:43 +0000

    Created function to check iss times for my location

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+  printPassTimes(passTimes);
+});
+/*
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -28,3 +47,4 @@ fetchISSFlyOverTimes(myCoords, (error, data) => {
   }
   console.log('It worked! Returned ISS pass times: ', data);
 });
+*/
